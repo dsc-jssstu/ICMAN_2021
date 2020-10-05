300c3f95027376112e1a2354528d5fa71f0f6776
Poorvi <poorvishetty1202@gmail.com>
2020-10-06 01:58:01 +0530

    a draft

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+(function($) {
+  "use strict";
+
+//Timer
+  $('#clock-b').countdown('2021/12/18').on('update.countdown', function(event) {
+    var $this = $(this).html(event.strftime(''
+      + '<span class="h1 font-weight-bold">%D</span>Day%!d'
+      + '<span class="h1 font-weight-bold">%H</span> Hr'
+      + '<span class="h1 font-weight-bold">%M</span> Min'
+      + '<span class="h1 font-weight-bold">%S</span> Sec'));
+  });
+
+})(jQuery); 
